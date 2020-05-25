7d5a17708651c1b0d5b6cac0ffc876a3dacea0fd
Macoy Madson <macoymadson@gmail.com>
2020-05-25 16:39:23 -0700

    Added early dictionary integration

--- src/TextProcessor.cpp
+++ src/TextProcessor.cpp
@@ -1,7 +1,8 @@
 #include <mecab.h>
-#include <iostream>
-#include <fstream>
 #include <cstring>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 #define CHECK(eval)                                                        \
 	if (!eval)                                                             \
@@ -12,9 +13,59 @@
 		return -1;                                                         \
 	}
 
+static char* rawDictionary = nullptr;
+static size_t rawDictionarySize = 0;
+void loadDictionary()
+{
+	std::ifstream inputFile;
+	// std::ios::ate so tellg returns the size
+	inputFile.open("data/utf8Edict2", std::ios::in | std::ios::binary | std::ios::ate);
+	rawDictionarySize = inputFile.tellg();
+	rawDictionary = new char[rawDictionarySize];
+	inputFile.seekg(0, std::ios::beg);
+	inputFile.read(rawDictionary, rawDictionarySize);
+	inputFile.close();
+}
+
+// enum class EDict2ReadState
+// {
+// 	None = 0,
+// 	JapaneseWord,
+
+// }
+
+bool getDictionaryResults(const char* query, char* outBuffer, size_t outBufferSize)
+{
+	// TODO index
+	// for (size_t i = 0; i < rawDictionarySize; ++i)
+	// {
+	// if (rawDictionary[i] == '\n')
+	// readState = EDict2ReadState::None;
+	// }
+	char* result = strstr(rawDictionary, query);
+	if (result)
+	{
+		// TODO Range check raw Dictionary pointer
+		for (size_t i = 0; i < outBufferSize; i++)
+		{
+			outBuffer[i] = result[i];
+			if (result[i] == '\n')
+				break;
+		}
+		return true;
+	}
+	return false;
+}
+
+void freeDictionary()
+{
+	delete[] rawDictionary;
+}
+
 // Sample of MeCab::Tagger class.
 int main(int argc, char** argv)
 {
+	loadDictionary();
 	std::ifstream inputFile;
 	// std::ios::ate so tellg returns the size
 	inputFile.open("data/Test.org", std::ios::in | std::ios::binary | std::ios::ate);
@@ -26,8 +77,8 @@ int main(int argc, char** argv)
 
 	char* input = memblock;
 	// char input[1024] =
-	    // "\u592a\u90ce\u306f\u6b21\u90ce\u304c\u6301\u3063\u3066\u3044\u308b\u672c\u3092\u82b1\u5b50"
-	    // "\u306b\u6e21\u3057\u305f\u3002";
+	// "\u592a\u90ce\u306f\u6b21\u90ce\u304c\u6301\u3063\u3066\u3044\u308b\u672c\u3092\u82b1\u5b50"
+	// "\u306b\u6e21\u3057\u305f\u3002";
 
 	MeCab::Tagger* tagger = MeCab::createTagger("");
 	CHECK(tagger);
@@ -51,7 +102,12 @@ int main(int argc, char** argv)
 			feature[node->length] = '\0';
 			// for (int i = 0; i < node->length; ++i)
 			// feature[i] = *(node->surface + i);
-			std::cout << feature << "\n";
+
+			char dictionaryResult[512] = {0};
+			if (getDictionaryResults(feature, dictionaryResult, sizeof(dictionaryResult)))
+				std::cout << feature << " " << dictionaryResult << "\n";
+			else
+				std::cout << feature << "\n";
 		}
 
 		// std::cout << node->id << ' ';
@@ -72,6 +128,8 @@ int main(int argc, char** argv)
 	delete tagger;
 
 	delete[] memblock;
+
+	freeDictionary();
 
 	return 0;
 }
